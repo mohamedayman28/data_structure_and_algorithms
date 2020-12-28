15b26b9dd82d2ceb8eedb8eed537c3dcea0b3700
mohamedayman28 <mohamedaymanhassenabas42@gmail.com>
2020-12-28 14:26:13 +0200

    Clean the code.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,28 +1,30 @@
 """ Python program for implementation of Bubble Sort.
-Code from: https://www.geeksforgeeks.org/bubble-sort/"""
+Code originally from: https://www.geeksforgeeks.org/bubble-sort/"""
 
 
 def bubble_sort(arr):
     """ Time Complexity O(n ** 2)"""
 
     n = len(arr)
+
     # Show steps during iterations, and not mandatory for Bubble Sort.
     outer_loop_result = 0
     inner_loop_result = 0
 
-    # Outer loop.
-    for out_step in range(n):
+    # Outer loop, loop over all elements in the array.
+    for outer_loop in range(n):
         outer_loop_result += 1
         inner_loop_result = 0
         print(f"Result: {outer_loop_result} ==> {arr}")
 
         # Inner loop.
-        # Loop is (n - 2) to avoid out of range when swap last two elements.
-        for in_step in range(n - out_step - 1):
-            # If left element greater than right element.
-            if arr[in_step] > arr[in_step + 1]:
-                # Swap elements.
-                arr[in_step], arr[in_step + 1] = arr[in_step + 1], arr[in_step]
+        # NOTE: Loop is (n - 2) to avoid index out of range when swap last two
+        # elements.
+        for inner_loop in range(n - outer_loop - 1):
+            # If left element greater than its next element, swap them.
+            if arr[inner_loop] > arr[inner_loop + 1]:
+                arr[inner_loop],
+                arr[inner_loop + 1] = arr[inner_loop + 1], arr[inner_loop]
 
             inner_loop_result += 1
             print(f"    Inner: {inner_loop_result} ==> {arr}")
