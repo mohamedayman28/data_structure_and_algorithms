69d7a9074429ea6cb6cd13afcfc3fe60e0a9f71c
mohamedayman28 <mohamedaymanhassenabas42@gmail.com>
2020-12-28 09:11:50 +0200

    Comments to clarify steps.

--- data_structure/bubble_sort.py
+++ data_structure/bubble_sort.py
@@ -4,26 +4,34 @@ Code from: https://www.geeksforgeeks.org/bubble-sort/"""
 
 def bubble_sort(arr):
     """ Time Complexity O(n ** 2)"""
+
     n = len(arr)
+    # Show steps during iterations, and not mandatory for Bubble Sort.
+    outer_loop_result = 0
+    inner_loop_result = 0
 
-    # Last i elements are already in place.
-    for i in range(n):
+    # Outer loop.
+    for out_step in range(n):
+        outer_loop_result += 1
+        inner_loop_result = 0
+        print(f"Result: {outer_loop_result} ==> {arr}")
 
-        # traverse the array from 0 to n-i-1
-        # Swap if the element found is greater
-        # than the next element
-        for j in range(n - i - 1):
-            print(n - i - 1)
-            if arr[j] > arr[j + 1]:
+        # Inner loop.
+        # Loop is (n - 2) to avoid out of range when swap last two elements.
+        for in_step in range(n - out_step - 1):
+            # If left element greater than right element.
+            if arr[in_step] > arr[in_step + 1]:
                 # Swap elements.
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr[in_step], arr[in_step + 1] = arr[in_step + 1], arr[in_step]
+
+            inner_loop_result += 1
+            print(f"    Inner: {inner_loop_result} ==> {arr}")
 
 
-# Driver code to test above
-arr = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+arr = [5, 3, 1, 9, 8, 2, 4, 7]
 
 bubble_sort(arr)
 
-print("Sorted array is:")
+print((5 * '\n') + "Sorted array is:")
 for i in arr:
     print(i)
